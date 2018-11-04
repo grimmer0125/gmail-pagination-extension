6761b193aafe1f0cae999f37a6f0c4baecfc8bcd
Grimmer <grimmer0125@gmail.com>
2018-11-04 19:42:29 +0800

    Add a slider to navigate other pages in Gmail

--- public/background.js
+++ public/background.js
@@ -1,15 +1,16 @@
-chrome.tabs.onUpdated.addListener(function(tabId, changeInfo, tab) {
-    console.log("on update event"); //many times
+chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
+  console.log('on update event'); // many times
 
-    console.log("changeInfo:",changeInfo);
-    // console.log("info url:",changeInfo.url);  // it only has value when status == loading
-    console.log("tab:",tab.url);
+  console.log('changeInfo:', changeInfo);
+  // console.log("info url:",changeInfo.url);  // it only has value when status == loading
+  console.log('tab:', tab.url);
 
-    // send query # hash request, onUpdated will be triggered several times
-    if (changeInfo.status === "complete") {
-        chrome.tabs.sendMessage( tabId, {
-                message: 'hello!',
-                url: tab.url
-            })
-    }
+  // send query # hash request, onUpdated will be triggered several times
+  if (changeInfo.status === 'complete') {
+    // tabID, request
+    chrome.tabs.sendMessage(tabId, {
+      message: 'tab_update_completed',
+      url: tab.url,
+    });
+  }
 });
