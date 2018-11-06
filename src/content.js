32adbaedd2ad55a02763f9885d1cb76b8b492283
Grimmer <grimmer0125@gmail.com>
2018-11-06 15:12:47 +0800

    Get label/catelog from url to reset count when switching
    Navigating to some page works for labe/catelog now.

--- src/content.js
+++ src/content.js
@@ -9,8 +9,6 @@ import Button from '@material-ui/core/Button';
 
 import './content.css';
 
-console.log('contentScript starts');
-
 const { chrome } = window;
 
 const styles = theme => ({
@@ -25,7 +23,7 @@ const styles = theme => ({
   },
 });
 
-function findCountInfo(node, type) {
+function findCountInfo(node) {
   const countInfoSpan = node.querySelectorAll('.Dj');
   if (countInfoSpan) {
     if (countInfoSpan.length >= 1) {
@@ -60,6 +58,50 @@ function findCountInfo(node, type) {
   }
 }
 
+function fetchLabelPart(str) {
+  let r = null;
+
+  r = str.match(/.*\/#category\/(.*)\//);
+  if (r) {
+    // https://mail.google.com/mail/u/0/#category/promotions/p2
+    console.log('label type6 (#category/promotions/p2):', r[1]);
+    return r[1];
+  }
+  r = str.match(/.*\/#category\/(.*)/);
+  if (r) {
+    console.log('label type5 (#category/promotions):', r[1]);
+    return r[1];
+  }
+
+  r = str.match(/.*\/#(.*)\//);
+  if (!r) {
+    // #inbox type
+    r = str.match(/.*\/#(.*)/);
+    // console.log('label type1(#inbox):', r[1]);
+  } else {
+    //   console.log('debug1:', r[1]); e.g. search, inbox/p1, label/apple
+    if (r[1].indexOf('label') > -1) {
+      r = str.match(/.*\/#label\/(.*)\//);
+      if (!r) {
+        // #label/104-32'
+        r = str.match(/.*\/#label\/(.*)/);
+        // console.log('label type3:', r[1]);
+      } else {
+        // #label/104-32/p1
+        // console.log('label type4:', r[1]);
+      }
+    } else {
+      // console.log('label type2(#search or inbox/p*):', r[1]);
+    }
+  }
+
+  if (r && r[1]) {
+    return r[1];
+  }
+
+  return null;
+}
+
 class SimpleSlider extends React.Component {
   constructor(props) {
     super(props);
@@ -72,20 +114,50 @@ class SimpleSlider extends React.Component {
 
     this.threadsPerPage = 0;
     this.detectedNewUI = false;
+    this.label = null;
   }
+
 
   componentDidMount() {
     // url part
     chrome.runtime.onMessage.addListener((request) => {
       if (request.message === 'tab_update_completed') {
         const url = window.location.href;
+
+        const label = fetchLabelPart(url);
+        if (label) {
+          if (this.label !== label) {
+            console.log('reset total, new label:', label);
+            this.label = label;
+            // reset total
+            this.setState({
+              countData: {},
+            });
+          }
+        } else {
+          console.log('can not find out label from url');
+        }
+
+        // if (isNaN(currentPage)) {
+        //   const pageStr = url.replace(/.*\/#(?!search).*\/p/g, '');
+        //   currentPage = parseInt(pageStr, 10);
+        // }
+
+        // TODO:
+        // 1 x works: https://mail.google.com/mail/u/0/#category/promotions/p2, https://mail.google.com/mail/u/0/#category/social/p2
+        // 2 x https://mail.google.com/mail/u/0/#category/promotions //NaN, https://mail.google.com/mail/u/0/#category/social
+        // 3 search/advanced-search case, e.g.
+        // https://mail.google.com/mail/u/0/#advanced-search/subset=all&has=apple&hasnot=pay&within=1d&sizeoperator=s_sl&sizeunit=s_smb&query=apple
         const pageStr = url.replace(/.*\/#(?!search).*\/p/g, '');
         let currentPage;
         if (pageStr !== url) {
           currentPage = parseInt(pageStr, 10);
+          if (isNaN(currentPage)) {
+            currentPage = 1; // https://mail.google.com/mail/u/0/#category/promotions
+          }
           console.log('set currentpage from url:', currentPage);
         } else if (url.match(/.*\/#search.*\//g) || url.match(/.*\/#advanced-search.*\//g)) {
-          currentPage = 0;
+          currentPage = 0; // search type, ignore to handle on UI
         } else {
           currentPage = 1; // it might be no email
         }
@@ -96,7 +168,7 @@ class SimpleSlider extends React.Component {
     });
 
     // span part
-    const observer = new MutationObserver(((mutations, me) => {
+    const observer = new MutationObserver((() => {
       let newCountData = null;
       newCountData = findCountInfo(document);
 
@@ -131,7 +203,32 @@ class SimpleSlider extends React.Component {
       const match = url.match(/.*\/#.*\/p/g);
       let newUrl;
       if (match) {
-        newUrl = match + currentPage;
+
+        const matchCatelog = url.match(/.*\/#category\/promotions/g);
+
+        if (matchCatelog) {
+          // #category\/promotions or #category\/promotions/p3
+          const match2 = url.match(/.*\/#category\/promotions\/p/g);
+          if (match2) {
+            newUrl = match2[0] + currentPage;
+          } else {
+            newUrl = `${url}/p${currentPage}`;
+          }
+        } else {
+          // handle some label starts from p, e.g. #label/p*
+          const matchLabel = url.match(/.*\/#label\/p.*/g);
+
+          if (matchLabel) {
+            const match3 = url.match(/.*\/#label\/p.*\/p/g);
+            if (match3) {
+              newUrl = match3[0] + currentPage;
+            } else {
+              newUrl = `${url}/p${currentPage}`;
+            }
+          } else {
+            newUrl = match[0] + currentPage;
+          }
+        }
       } else {
         newUrl = `${url}/p${currentPage}`;
       }
@@ -144,17 +241,15 @@ class SimpleSlider extends React.Component {
     this.setState({ currentPage: value });
   };
 
+
   render() {
     const { classes } = this.props;
     const { currentPage, countData, usngNewUI } = this.state;
-
     if (usngNewUI) {
       return (
         <div className="flex-container">
-          <div>
-            <Button variant="outlined" color="secondary" className={classes.button}>
-          New Gmail (multiple tab) not supported. Please uncheck some tabs (e.g. social, promotions) in the setting -> configure inbox.
-            </Button>
+          <div style={{ color: 'red' }}>
+          New Gmail default UI (Catagories on tabs) does not support pagination. Please uncheck some tabs (e.g. social, promotions) in the setting -> configure inbox. You can still access catagories in the left menu.
           </div>
         </div>
       );
@@ -226,8 +321,6 @@ function addExtensionUI() {
 }
 
 chrome.storage.sync.get(['visibility'], (result) => {
-  console.log(`Value currently is ${result.visibility}`);
-
   if (!result.visibility || result.visibility === 'show') {
     addExtensionUI();
   }
